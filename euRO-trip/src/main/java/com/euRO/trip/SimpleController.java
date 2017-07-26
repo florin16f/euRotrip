e0c6719cd37e9c51ffce662b58722f3e3c67e177
Florin Fometescu <florin16f@gmail.com>
2017-07-26 16:15:28 +0300

    added methods to submit and receive the form

--- euRO-trip/src/main/java/com/euRO/trip/SimpleController.java
+++ euRO-trip/src/main/java/com/euRO/trip/SimpleController.java
@@ -1,14 +1,19 @@
 package com.euRO.trip;
 
+import com.euRO.trip.model.Form;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.IOException;
 import java.util.Map;
 
 
 @Controller
 public class SimpleController {
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getHome(Map<String, Object> model) {
@@ -24,14 +29,28 @@ public class SimpleController {
     public String getOrase(Map<String, Object> model) {
         return "orase";
     }
+
     @RequestMapping(value = "contact", method = RequestMethod.GET)
     public String getContact(Map<String, Object> model) {
         return "contact";
     }
 
-    @RequestMapping(value = "contact_message", method = RequestMethod.GET)
-    public String getContactMessage(Map<String, Object> model) {
-        return "contact_message";
+    @RequestMapping(value = "form", method = RequestMethod.GET)
+        public String getContactMessage(Map<String, Object> model) {
+            Form form = null;
+            try {
+                form = objectMapper.readValue(this.getClass().getClassLoader().getResourceAsStream("form.json"), Form.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        model.put("form", form);
+        return "form";
+    }
+
+    @RequestMapping(value = "form", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute Form form) {
+        MailSender.sendEmail("Form", form.toString());
+        return "formSucces";
     }
 
     @RequestMapping(value = "timisoara", method = RequestMethod.GET)
