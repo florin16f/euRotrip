b04a02ccadda765f917bebf626c436cc9e193220
Florin Fometescu <florin16f@gmail.com>
2017-07-27 11:51:39 +0300

    update

--- euRO-trip/src/main/java/com/euRO/trip/FormController.java
+++ euRO-trip/src/main/java/com/euRO/trip/FormController.java
@@ -1,15 +1,20 @@
 package com.euRO.trip;
 
 import com.euRO.trip.model.Form;
+import com.euRO.trip.model.FormService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.InputStream;
 import java.util.Map;
 
 @Controller
 public class FormController {
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @RequestMapping(value = "form", method = RequestMethod.GET)
     public String getContactMessage(Map<String, Object> model) {
@@ -20,7 +25,9 @@ public class FormController {
 
     @RequestMapping(value = "form", method = RequestMethod.POST)
     public String submitForm(@ModelAttribute Form form) {
-        MailSender.sendEmail("Form", form.toString());
+        FormService formService = new FormService();
+        formService.processForm(form);
+        //MailSender.sendEmail("Form", form.toString());
         return "formSucces";
     }
 }
