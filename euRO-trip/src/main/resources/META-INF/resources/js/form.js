d205e2c60a7c297042ff19aa9adbb5dcb3d7a0e9
Florin Fometescu <florin16f@gmail.com>
2017-08-01 13:03:09 +0300

    update 2

--- euRO-trip/src/main/resources/META-INF/resources/js/form.js
+++ euRO-trip/src/main/resources/META-INF/resources/js/form.js
@@ -1,34 +1,41 @@
 $( document ).ready(function() {
-    var frm = $('#question-form');
+          var frm = $('#question-form');
 
-    frm.submit(function (e) {
+          frm.submit(function (e) {
 
-        e.preventDefault();
+              e.preventDefault();
 
-        $.ajax({
-            type: frm.attr('method'),
-            url: frm.attr('action'),
-            data: frm.serialize(),
-            success: function (data) {
-                console.log('Submission was successful.');
-                console.log(data);
-            },
-            error: function (data) {
-                console.log('An error occurred.');
-                console.log(data);
-            },
-        });
-    });
-   $.getJSON( "/files/form.json", function( data ) {
-        var items = [];
-        items.push( "<input type=\"hidden\" value="+data.question+" name=\"question\">"+data.question);
-        for (var i in data.answers) {
-            var val = data.answers[i];
-            items.push("<input type=\"checkbox\" name=\"answers\" value="+val+">"+val);
-          };
-        $( "<div/>", {
-          "class": "my-new-list",
-          html: items.join( "" )
-        }).appendTo( "body" );
-      });
-})
+              $.ajax({
+                  type: frm.attr('method'),
+                  url: frm.attr('action'),
+                  data: frm.serialize(),
+                  success: function (data) {
+                      console.log('Submission was successful.');
+                      console.log(data);
+                  },
+                  error: function (data) {
+                      console.log('An error occurred.');
+                      console.log(data);
+                  },
+              });
+          });
+
+
+         $.getJSON( '@RequestMapping(value = "api/login", method = RequestMethod.GET, produces = "application/json")', function( data ) {
+              var items = [];
+              items.push( "<input type=\"hidden\" value="+data.question+" name=\"question\">"+data.question);
+              for (var i in data.answers) {
+                  var val = data.answers[i];
+                  items.push("<input type=\"checkbox\" name=\"answers\" value="+val+">"+val);
+                };
+              $( "<div/>", {
+                "class": "my-new-list",
+                html: items.join( "" )
+              }).appendTo( "body" );
+            });
+      })
+
+
+
+
+
